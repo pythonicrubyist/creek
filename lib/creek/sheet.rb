bfedfa4197d37e174ab95af94b630ba7e0a69983
Ilyas Garaev <ilyas.garaev@flatstack.com>
2016-04-14 12:33:13 +0300

    refactor Creek::Sheet class

--- lib/creek/sheet.rb
+++ lib/creek/sheet.rb
@@ -21,13 +21,6 @@ module Creek
       @rid = rid
       @state = state
       @sheetfile = sheetfile
-
-      # An XLS file has only 256 columns, however, an XLSX or XLSM file can contain up to 16384 columns.
-      # This function creates a hash with all valid XLSX column names and associated indices.
-      @excel_col_names = Hash.new
-      (0...16384).each do |i|
-        @excel_col_names[col_name(i)] = i
-      end
     end
 
     ##
@@ -45,13 +38,6 @@ module Creek
     end
 
     private
-    ##
-    # Returns valid Excel column name for a given column index.
-    # For example, returns "A" for 0, "B" for 1 and "AQ" for 42.
-    def col_name i
-      quot = i/26
-      (quot>0 ? col_name(quot-1) : "") + (i%26+65).chr
-    end
 
     ##
     # Returns a hash per row that includes the cell ids and values.
@@ -64,7 +50,7 @@ module Creek
         opener = Nokogiri::XML::Reader::TYPE_ELEMENT
         closer = Nokogiri::XML::Reader::TYPE_END_ELEMENT
         Enumerator.new do |y|
-          shared, row, cells, cell = false, nil, {}, nil
+          row, cells, cell = nil, {}, nil
           cell_type  = nil
           cell_style_idx = nil
           @book.files.file.open(path) do |xml|
@@ -82,9 +68,8 @@ module Creek
                 cell_type      = node.attributes['t']
                 cell_style_idx = node.attributes['s']
                 cell           = node.attributes['r']
-
               elsif (node.name.eql? 'v') and (node.node_type.eql? opener)
-                if !cell.nil?
+                unless cell.nil?
                   cells[cell] = convert(node.inner_xml, cell_type, cell_style_idx)
                 end
               end
@@ -106,22 +91,18 @@ module Creek
     ##
     # The unzipped XML file does not contain any node for empty cells.
     # Empty cells are being padded in using this function
-    def fill_in_empty_cells cells, row_number, last_col
+    def fill_in_empty_cells(cells, row_number, last_col)
       new_cells = Hash.new
+
       unless cells.empty?
-        keys = cells.keys.sort
         last_col = last_col.gsub(row_number, '')
-        last_col_index = @excel_col_names[last_col]
-        [*(0..last_col_index)].each do |i|
-          col = col_name i
-          id = "#{col}#{row_number}"
-          unless cells.has_key? id
-              new_cells[id] = nil
-          else
-            new_cells[id] = cells[id]
-          end
+
+        ("A"..last_col).to_a.each do |column|
+          id = "#{column}#{row_number}"
+          new_cells[id] = cells[id]
         end
       end
+
       new_cells
     end
   end
