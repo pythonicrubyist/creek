a7099c6193146c0beb1506b509a601490097836d
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2021-11-03 22:39:31 -0500

    Include XML namespace when constructing the shared strings dictionary

--- spec/shared_string_spec.rb
+++ spec/shared_string_spec.rb
@@ -17,4 +17,21 @@ describe 'shared strings' do
     expect(dictionary[6]).to eq('吉田兼好')
   end
 
+  context 'when the nodes are namespaced' do
+    it 'parses the dictionary correctly' do
+      shared_strings_xml_file = File.open('spec/fixtures/sst_namespaced.xml')
+      doc = Nokogiri::XML(shared_strings_xml_file)
+      dictionary = Creek::SharedStrings.parse_shared_string_from_document(doc)
+
+      expect(dictionary.keys.size).to eq(7)
+      expect(dictionary[0]).to eq('Cell A1')
+      expect(dictionary[1]).to eq('Cell B1')
+      expect(dictionary[2]).to eq('My Cell')
+      expect(dictionary[3]).to eq('Cell A2')
+      expect(dictionary[4]).to eq('Cell B2')
+      expect(dictionary[5]).to eq("Cell with\rescaped\rcharacters")
+      expect(dictionary[6]).to eq('吉田兼好')
+    end
+
+  end
 end
