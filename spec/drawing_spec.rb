7d74b28a373c577832eb343dbc7a7bbd78f705ea
PavitKaur05 <pavitk1@gmail.com>
2022-10-29 04:25:54 +0530

    Support for parsing one cell anchored images

--- spec/drawing_spec.rb
+++ spec/drawing_spec.rb
@@ -3,9 +3,11 @@ require './spec/spec_helper'
 describe 'drawing' do
   let(:book) { Creek::Book.new('spec/fixtures/sample-with-images.xlsx') }
   let(:book_no_images) { Creek::Book.new('spec/fixtures/sample.xlsx') }
+  let(:book_with_one_cell_anchored_images) { Creek::Book.new('spec/fixtures/sample-with-one-cell-anchored-images.xlsx') }
   let(:drawingfile) { 'xl/drawings/drawing1.xml' }
   let(:drawing) { Creek::Drawing.new(book, drawingfile) }
   let(:drawing_without_images) { Creek::Drawing.new(book_no_images, drawingfile) }
+  let(:drawing_with_one_cell_anchored_images) { Creek::Drawing.new(book_with_one_cell_anchored_images, drawingfile) }
 
   describe '#has_images?' do
     it 'has' do
@@ -46,6 +48,22 @@ describe 'drawing' do
         image2 = drawing.images_at('A5')[0]
         expect(image1.class).to eq(Pathname)
         expect(image1).to eq(image2)
+      end
+    end
+
+    context 'when one cell anchored images in cell' do
+      it 'returns image for anchored cell' do
+        image = drawing_with_one_cell_anchored_images.images_at('A2')[0]
+        expect(image.class).to eq(Pathname)
+        expect(image.exist?).to eq(true)
+      end
+
+      it 'returns nil for non-anchored cell' do
+        image = drawing_with_one_cell_anchored_images.images_at('A3')[0]
+        # Image can be seen present on cell A4 in `sample-with-one-cell-anchored-images.xlsx`
+        image_at_non_anchored_cell = drawing_with_one_cell_anchored_images.images_at('A4')
+        expect(image.class).to eq(Pathname)
+        expect(image_at_non_anchored_cell).to eq(nil)
       end
     end
   end
